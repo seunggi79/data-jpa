c65a41a195089cbee705479612eb1463ac0e3840
seunggi <hsg77689469@gmail.com>
2026-02-12 16:42:19 +0900

    JPA NamedQuery

--- src/main/java/study/data_jpa/repository/MemberRepository.java
+++ src/main/java/study/data_jpa/repository/MemberRepository.java
@@ -1,10 +1,15 @@
 package study.data_jpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.data_jpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    @Query(name = "Member.findByUsername") //생략 가능
+    List<Member> findByUsername(@Param("username") String username);
 }
