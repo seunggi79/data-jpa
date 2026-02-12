035758e1c2c683e1f2b658728f2b3da06ef656b7
seunggi <hsg77689469@gmail.com>
2026-02-12 12:47:00 +0900

    스프링 데이터 JPA와 DB 설정, 동작확인

---
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,30 @@
+package study.data_jpa.repository;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.data_jpa.entity.Member;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+public class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberRepository.save(member);
+        Member findMember =
+                memberRepository.findById(savedMember.getId()).get();
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+
+        Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername()
+        );
+        Assertions.assertThat(findMember).isEqualTo(member); //JPA 엔티티 동일성 보장
+    }
+}
