7ac9c2bec7e9d26448bab40ef80e12d371d5c950
seunggi <hsg77689469@gmail.com>
2026-02-13 15:16:46 +0900

    Web 확장 - 페이징과 정렬

--- src/main/java/study/data_jpa/controller/MemberController.java
+++ src/main/java/study/data_jpa/controller/MemberController.java
@@ -2,9 +2,12 @@ package study.data_jpa.controller;
 
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.data_jpa.dto.MemberDto;
 import study.data_jpa.entity.Member;
 import study.data_jpa.repository.MemberRepository;
 
@@ -23,8 +26,15 @@ public class MemberController {
         return member.getUsername();
     }
 
+    @GetMapping("/members")
+    public Page<MemberDto> list(Pageable pageable) {
+        return memberRepository.findAll(pageable).map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init(){
-        memberRepository.save(new Member("memberA"));
+        for(int i =0; i<100; i++){
+            memberRepository.save(new Member("memberA"+i, i));
+        }
     }
 }
---
+++ src/main/java/study/data_jpa/dto/MemberDto.java
@@ -0,0 +1,14 @@
+package study.data_jpa.dto;
+
+import lombok.Data;
+import study.data_jpa.entity.Member;
+
+@Data
+public class MemberDto {
+    private Long id;
+    private String username;
+    public MemberDto(Member m) {
+        this.id = m.getId();
+        this.username = m.getUsername();
+    }
+}
